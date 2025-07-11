314578fc83dbe43b53703e19db6b927f89fcf693
mehmet.gok1 <mehmetgk2015@gmail.com>
2025-07-12 02:01:56 +0300

    working example but inductor is too large

--- matlab_code/parameters.m
+++ matlab_code/parameters.m
@@ -1,2 +1,14 @@
-delay =1000e-6;
-freq=1e6;
+%% Frequency and Sampling Specs
+buck_freq=1e5;
+sampling=1e6;
+delay =100/sampling;
+%% MPPT Points
+Vin=30.6;
+%% Expeted Output
+Vout=15;
+D=Vout/Vin;
+%% Inductor Sizing
+Ripple_Iout=0.2;
+Lout=Vout*(1-D)/(buck_freq*Ripple_Iout);
+%% Capacitor Sizing
+Cin=10e-3;
